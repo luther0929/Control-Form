0f2b6c1b09e404e4c12fd41a4025cab7150e69fb
Your Name <evangelista.luther1995@gmail.com>
2025-07-09 12:18:48 +1000

    added Get, Put, and  Delete endpoints in backend

--- backend/main.go
+++ backend/main.go
@@ -20,6 +20,7 @@ func main() {
 
 	app.Post("/submit", func(c *fiber.Ctx) error {
 		var control Control
+
 		if err := c.BodyParser(&control); err != nil {
 			return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
 		}
@@ -40,11 +41,69 @@ func main() {
 			"Monitoring":      true,
 		}
 		if !validCategories[control.Category] {
-			return c.Status(400).JSON(fiber.Map{"error": "Invalid Category"})
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid category"})
 		}
 
 		controls = append(controls, control)
-		return c.JSON(fiber.Map{"message": "Submission successful", "control": control})
+
+		return c.JSON(fiber.Map{"message": "Submission successful", "deletedControl": control})
+	})
+
+	app.Get("/controls", func(c *fiber.Ctx) error {
+		return c.JSON(controls)
+	})
+
+	app.Put("/controls/:id", func(c *fiber.Ctx) error {
+		controlId := c.Params("id")
+		var updatedControl Control
+
+		if err := c.BodyParser(&updatedControl); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
+		}
+
+		if updatedControl.ControlID == "" || updatedControl.Category == "" || updatedControl.Description == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "All fields are required"})
+		}
+		controlIdRegex := regexp.MustCompile(`^CTRL-\d{3}$`)
+		if !controlIdRegex.MatchString(updatedControl.ControlID) {
+			return c.Status(400).JSON(fiber.Map{"error": "Control ID must be format CTRL-XXX"})
+		}
+		if len(updatedControl.Description) < 10 || len(updatedControl.Description) > 500 {
+			return c.Status(400).JSON(fiber.Map{"error": "Description must be 10-500 characters"})
+		}
+		validCategories := map[string]bool{
+			"Access Control":  true,
+			"Data Protection": true,
+			"Monitoring":      true,
+		}
+		if !validCategories[updatedControl.Category] {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid category"})
+		}
+
+		for i, control := range controls {
+			if control.ControlID == controlId {
+				controls[i] = updatedControl
+
+				return c.JSON(fiber.Map{"message": "Control updated successfully", "updatedControl": updatedControl})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Control not found"})
+
+	})
+
+	app.Delete("/controls/:id", func(c *fiber.Ctx) error {
+		controlId := c.Params("id")
+
+		for i, control := range controls {
+			if controlId == control.ControlID {
+				deletedControl := controls[i]
+				controls = append(controls[:i], controls[i+1:]...)
+
+				return c.JSON(fiber.Map{"message": "Control deleted successfully", "control": deletedControl})
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "Control not found"})
 	})
 
 	if err := app.Listen(":3000"); err != nil {
